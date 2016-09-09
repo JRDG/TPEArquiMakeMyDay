b8bb5627f192483c382112df1073b0b1a6bfe138
JRDG <joarod95@gmail.com>
2016-09-09 17:08:04 -0300

    Completada la entidad Historial_Usuario

--- TPE - Arqui Web - MakeMyDay- Parte1/src/Entidades/Historial_Usuario.java
+++ TPE - Arqui Web - MakeMyDay- Parte1/src/Entidades/Historial_Usuario.java
@@ -1,5 +1,46 @@
 package Entidades;
+import java.sql.Date;
+import javax.persistence.*;
 
+@Entity
 public class Historial_Usuario {
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	@Column(nullable = false)
+	private Usuario usuario;
+	@Column(nullable = false)
+	private Actividad actividad;
+	@Column(nullable = false)
+	private double nivelFelicidad;
+	@Column(nullable = false)
+	private boolean privado;
+	private Date fecha_fin;
+	
+	public Historial_Usuario(Usuario usuario, Actividad actividad, double nivelFelicidad, boolean privado) {
+		super();
+		this.usuario = usuario;
+		this.actividad = actividad;
+		this.nivelFelicidad = nivelFelicidad;
+		this.privado = privado;
+	}
+	public Date getFecha_fin() {
+		return fecha_fin;
+	}
+	public void setFecha_fin(Date fecha_fin) {
+		this.fecha_fin = fecha_fin;
+	}
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public Actividad getActividad() {
+		return actividad;
+	}
+	public double getNivelFelicidad() {
+		return nivelFelicidad;
+	}
+	public boolean isPrivado() {
+		return privado;
+	}
+	
 }
