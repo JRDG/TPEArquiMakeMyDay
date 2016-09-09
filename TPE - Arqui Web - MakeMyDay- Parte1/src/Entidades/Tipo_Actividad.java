30d04eb0f4f671fc9a4e22e4302e354ee898455f
whitefox <whitefox@Note-PBL2021>
2016-09-09 16:25:58 -0300

    Primera parte de la clase Tipo_Actividad

--- TPE - Arqui Web - MakeMyDay- Parte1/src/Entidades/Tipo_Actividad.java
+++ TPE - Arqui Web - MakeMyDay- Parte1/src/Entidades/Tipo_Actividad.java
@@ -1,5 +1,31 @@
 package Entidades;
 
-public class Tipo_Actividad {
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Tipo_Actividad {
+	
+	private String nombre;
+	
+	@id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	
+	private int idTipo;
+	
+	
+	public Tipo_Actividad(String nombre){
+
+		
+		
+		@Column(nullable=false)
+		this.nombre=nombre;
+	}
+	
+	
+	
 }
