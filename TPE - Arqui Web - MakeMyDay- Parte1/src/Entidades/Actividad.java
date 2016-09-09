c6792539157a367fdd9f1198bc8df5ab2d8c8ac3
whitefox <whitefox@Note-PBL2021>
2016-09-09 16:42:27 -0300

    Finalizado de Tipo_Actividad y especificacion de Actividad

--- TPE - Arqui Web - MakeMyDay- Parte1/src/Entidades/Actividad.java
+++ TPE - Arqui Web - MakeMyDay- Parte1/src/Entidades/Actividad.java
@@ -1,5 +1,43 @@
 package Entidades;
 
+
+import java.util.ArrayList;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Actividad {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)	
+	private int idTipo;
+	
+	@Column(nullable=false)
+	private String nombre;
+	
+	@Column(nullable=false)
+	private ArrayList<Tipo_Actividad> ListadeTipo;
+
+	
+	public Actividad(String nombre, ArrayList<Tipo_Actividad> listadeTipo) {
+		this.nombre = nombre;
+		ListadeTipo = listadeTipo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public ArrayList<Tipo_Actividad> getListadeTipo() {
+		return ListadeTipo;
+	}
+
+
+	
+	
+	
 }
--- TPE - Arqui Web - MakeMyDay- Parte1/src/Entidades/Tipo_Actividad.java
+++ TPE - Arqui Web - MakeMyDay- Parte1/src/Entidades/Tipo_Actividad.java
@@ -1,32 +1,28 @@
 package Entidades;
 
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
+
 
 @Entity
 public class Tipo_Actividad {
-	
+	@Column(nullable=false)
 	private String nombre;
 	
-	@id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)	
 	private int idTipo;
 	
-	
-	
 	public Tipo_Actividad(String nombre){
-
-		
-		
-		@Column(nullable=false)
 		this.nombre=nombre;
 	}
 	
+	public String getNombre(){
+		return nombre;
+	}
+	public int getId(){
+		return idTipo;
+	}
 	
 	
 }
