ecd9e6a39c65133a3fa8d66a31ec552f9dc4a7ca
whitefox <whitefox@Note-PBL2021>
2016-09-09 17:37:58 -0300

    Agregado de la asociacion

--- TPE - Arqui Web - MakeMyDay- Parte1/src/Entidades/Actividad.java
+++ TPE - Arqui Web - MakeMyDay- Parte1/src/Entidades/Actividad.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Actividad {
@@ -19,10 +20,11 @@ public class Actividad {
 	@Column(nullable=false)
 	private String nombre;
 	
+	@ManyToMany
 	@Column(nullable=false)
 	private ArrayList<Tipo_Actividad> ListadeTipo;
 
-	
+
 	public Actividad(String nombre, ArrayList<Tipo_Actividad> listadeTipo) {
 		this.nombre = nombre;
 		ListadeTipo = listadeTipo;
