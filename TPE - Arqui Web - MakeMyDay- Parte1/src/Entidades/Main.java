c4f6ae80aed8e110a27042d30b670fc721f359e7
whitefox <whitefox@Note-PBL2021>
2016-09-09 18:17:56 -0300

    primer intento de almacenar en la base de datos

--- TPE - Arqui Web - MakeMyDay- Parte1/src/Entidades/Main.java
+++ TPE - Arqui Web - MakeMyDay- Parte1/src/Entidades/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("Hola mamá");
+			System.out.println("Hola mama"); 
+			Usuario usuario1 = new Usuario("Victor","Gonzalez","37621713",23,"kurama");
 	}
 
 }
