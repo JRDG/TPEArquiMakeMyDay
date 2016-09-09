fc9bad17dc23b7610e9d3efedce7c6139e5e764b
JRDG <joarod95@gmail.com>
2016-09-09 16:35:00 -0300

    Agregados getters/setters a la entidad usuario

--- TPE - Arqui Web - MakeMyDay- Parte1/src/Entidades/Usuario.java
+++ TPE - Arqui Web - MakeMyDay- Parte1/src/Entidades/Usuario.java
@@ -27,6 +27,54 @@ public class Usuario {
 		this.edad = edad;
 		this.password = password;
 	}
+
+	public int getId_usuario() {
+		return id_usuario;
+	}
+
+	public void setId_usuario(int id_usuario) {
+		this.id_usuario = id_usuario;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getDNI() {
+		return DNI;
+	}
+
+	public void setDNI(String dNI) {
+		DNI = dNI;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 	
 	
 
