bca2a3fd48fc1aa9f61f426fcd4de3b123c58088
JRDG <joarod95@gmail.com>
2016-09-09 16:32:40 -0300

    variables y metodos basicos de la entidad usuario

--- TPE - Arqui Web - MakeMyDay- Parte1/src/Entidades/Usuario.java
+++ TPE - Arqui Web - MakeMyDay- Parte1/src/Entidades/Usuario.java
@@ -1,5 +1,33 @@
 package Entidades;
+import javax.persistence.*;
 
+
+@Entity
 public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id_usuario;
+	@Column(nullable = false)
+	private String nombre;
+	@Column(nullable = false)
+	private String apellido;
+	@Column(nullable = false)
+	private String DNI;
+	@Column(nullable = false)
+	private int edad;
+	@Column(nullable = false)
+	private String password;
+	
+	public Usuario(int id_usuario, String nombre, String apellido, String dNI, int edad, String password) {
+		this.id_usuario = id_usuario;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		DNI = dNI;
+		this.edad = edad;
+		this.password = password;
+	}
+	
+	
 
 }
