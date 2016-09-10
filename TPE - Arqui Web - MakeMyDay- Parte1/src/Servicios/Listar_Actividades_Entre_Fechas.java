b9f110b0edc999f3c69b0b4e9e794febbec73c0e
snosei <snosei92@gmail.com>
2016-09-10 19:34:58 -0300

    Agregadas clases para satisfacer nuevos servicios

---
+++ TPE - Arqui Web - MakeMyDay- Parte1/src/Servicios/Listar_Actividades_Entre_Fechas.java
@@ -0,0 +1,10 @@
+package Servicios;
+
+public class Listar_Actividades_Entre_Fechas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ TPE - Arqui Web - MakeMyDay- Parte1/src/Servicios/Obtener_Estadisticas.java
@@ -0,0 +1,10 @@
+package Servicios;
+
+public class Obtener_Estadisticas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
