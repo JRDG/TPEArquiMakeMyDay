f5560389b048614cbd99a13677b8a535756ecbd8
whitefox <whitefox@Note-PBL2021>
2016-09-12 18:58:26 -0300

    Eliminar usando sentencias sql, falta secuencia para extraer los FK y eliminarlos primero

--- TPE - Arqui Web - MakeMyDay- Parte1/src/servicios/Eliminar_Datos_DB.java
+++ TPE - Arqui Web - MakeMyDay- Parte1/src/servicios/Eliminar_Datos_DB.java
@@ -1,23 +1,39 @@
 package servicios;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+import entidades.Usuario;
 
 
 public class Eliminar_Datos_DB {
 
 	public static void main(String[] args) {
-
-		EntityManagerFactory emfactory = null;	
+		EntityManagerFactory emfactory = null;		
 		EntityManager emanager = null;
 	
 		try {
-			
-			emfactory = Persistence.createEntityManagerFactory("objectdb:makemyday.sql;drop");
+			emfactory = Persistence.createEntityManagerFactory("TPE-MakeMyDay-JPA");
 			emanager = emfactory.createEntityManager();
-			
-			emanager.getTransaction().begin();		
+	
+			emanager.getTransaction().begin();	
+	
+	 
+		    String jpql1 = "SELECT CONCAT('drop table ',table_name,'; ') FROM information_schema.tables WHERE table_schema = 'makemyday';"; 
+//		    String jpql2 = "SELECT CONCAT('alter table ' ,table_name, ' DROP INDEX 'tablename FROM information_schema.keycolumnusage WHERE referencedtablename ='$t'"
+		    
+		    Query query = emanager.createNativeQuery(jpql1); 
+		    List<String> resultados = query.getResultList(); 
+		    for(String  r : resultados) { 
+		    	Query query2 = emanager.createNativeQuery(r);  
+		    	query2.executeUpdate();
+		    } 
+		     
+	
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
@@ -26,9 +42,7 @@ public class Eliminar_Datos_DB {
 			if (emfactory != null)
 				emfactory.close();
 		}
-	}			
-		
-		
+	}		
 		
 
 }
