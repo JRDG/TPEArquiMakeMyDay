342a6318610a4e5c63e9ea18cc06ed40dcd6a0ac
snosei <snosei92@gmail.com>
2016-09-13 16:49:58 -0300

    Cambios en ejecucion de metodos del main

--- TPE - Arqui Web - MakeMyDay- Parte1/src/entidades/Main.java
+++ TPE - Arqui Web - MakeMyDay- Parte1/src/entidades/Main.java
@@ -8,14 +8,13 @@ public class Main {
 		Llenar_DB c = new Llenar_DB();
 		c.inicializarFactory();
 		c.cargar(); //Necesita tener descomentada la linea 36 del persistence 
-		c.cerrarFactory();
-		/*Search_Utils s = new Search_Utils();
+		Search_Utils s = new Search_Utils();
 		s.inicializarFactory();
 		s.Listar_Actividades();
 		s.ListarUsuarios();
 		s.ListarActividadesEntreFechas();
 		s.ObtenerEstadisticas();
-		s.cerrarFactory();*/
+		s.cerrarFactory();
 	}
 
 }
