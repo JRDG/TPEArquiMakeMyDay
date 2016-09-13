be3b8fad74cb76c1831013984aaa1c70f8bcfdc0
Joa <Joa@DESKTOP-R8PLF3A>
2016-09-13 20:50:20 -0300

    Agregado el test de creacion de tipos de actividades

--- TPE - Arqui Web - MakeMyDay- Parte1/src/tests/TestCargarTipoActividades.java
+++ TPE - Arqui Web - MakeMyDay- Parte1/src/tests/TestCargarTipoActividades.java
@@ -2,13 +2,48 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import org.junit.Test;
+import org.junit.Before;
+import entidades.Tipo_Actividad;
 
 public class TestCargarTipoActividades {
 
+	private EntityManagerFactory emfactory;
+	private EntityManager emanager;
+	Tipo_Actividad outdoor;
+	Tipo_Actividad indoor;
+	Tipo_Actividad academico;
+	Tipo_Actividad deporte;
+	Tipo_Actividad ocio;
+	
+	@Before
+	public void init(){
+		emfactory = Persistence.createEntityManagerFactory("TPE-MakeMyDay-JPA");
+		
+		outdoor = new Tipo_Actividad("outdoor");
+		indoor = new Tipo_Actividad("indoor");
+		academico = new Tipo_Actividad("academico");
+		deporte = new Tipo_Actividad("deporte");
+		ocio = new Tipo_Actividad("ocio");
+	}
+	
+	
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		emanager = emfactory.createEntityManager();
+		emanager.getTransaction().begin();
+		
+		emanager.persist(indoor);
+		emanager.persist(academico);
+		emanager.persist(ocio);
+		emanager.persist(outdoor);
+		emanager.persist(deporte);
+		
+		emanager.getTransaction().commit();
 	}
 
 }
