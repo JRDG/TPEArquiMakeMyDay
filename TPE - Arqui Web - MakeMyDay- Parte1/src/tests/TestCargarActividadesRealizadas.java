e15710ce4142f104e1d2441d7c3ac03772a29faf
snosei <snosei92@gmail.com>
2016-09-13 20:40:58 -0300

    Divididos los test de carga

---
+++ TPE - Arqui Web - MakeMyDay- Parte1/src/tests/TestCargarActividadesRealizadas.java
@@ -0,0 +1,14 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCargarActividadesRealizadas {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
