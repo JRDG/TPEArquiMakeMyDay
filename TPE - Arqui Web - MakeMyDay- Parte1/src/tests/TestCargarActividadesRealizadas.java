45e55817f7a55f201b7dfb0a3497d6faeb6d437a
whitefox <whitefox@Note-PBL2021>
2016-09-13 21:12:08 -0300

    actividades y tipos agregados

--- TPE - Arqui Web - MakeMyDay- Parte1/src/tests/TestCargarActividadesRealizadas.java
+++ TPE - Arqui Web - MakeMyDay- Parte1/src/tests/TestCargarActividadesRealizadas.java
@@ -2,6 +2,7 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
@@ -15,12 +16,18 @@ import org.junit.Test;
 
 import entidades.Actividad;
 import entidades.Actividad_Realizada;
+import entidades.Tipo_Actividad;
 
 public class TestCargarActividadesRealizadas {
 	
 	private EntityManagerFactory emfactory;
 	private EntityManager emanager;
 
+	Tipo_Actividad outdoor;
+	Tipo_Actividad indoor;
+	Tipo_Actividad academico;
+	Tipo_Actividad deporte;
+	Tipo_Actividad ocio;
 	
 	Actividad a1;
 	Actividad a2;
@@ -59,7 +66,57 @@ public class TestCargarActividadesRealizadas {
 	public void inicializacion(){
 		
 		emfactory = Persistence.createEntityManagerFactory("TPE-MakeMyDay-JPA");
-	
+
+		//tipos
+		outdoor = new Tipo_Actividad("outdoor");
+		indoor = new Tipo_Actividad("indoor");
+		academico = new Tipo_Actividad("academico");
+		deporte = new Tipo_Actividad("deporte");
+		ocio = new Tipo_Actividad("ocio");		
+		
+		//actividades
+		a1 = new Actividad("Cazar Pokemon", new ArrayList<Tipo_Actividad>(){{
+			add(ocio);
+			add(outdoor);
+		}});
+		a2 = new Actividad("Comer Helado", new ArrayList<Tipo_Actividad>(){{
+			add(ocio);
+			add(outdoor);
+		}});
+		a3 = new Actividad("Jugar al futbol", new ArrayList<Tipo_Actividad>(){{
+			add(ocio);
+			add(outdoor);
+			add(deporte);
+		}});
+		a4 = new Actividad("Trabajar", new ArrayList<Tipo_Actividad>(){{
+			add(indoor);
+		}});
+		a5 = new Actividad("Ir a cursar", new ArrayList<Tipo_Actividad>(){{
+			add(indoor);
+			add(academico);
+		}});
+		a6 = new Actividad("Comer facturas", new ArrayList<Tipo_Actividad>(){{
+			add(ocio);
+			add(indoor);
+		}});
+		a7 = new Actividad("Hacer el tpe de arqui", new ArrayList<Tipo_Actividad>(){{
+			add(indoor);
+			add(academico);
+		}});
+		a8 = new Actividad("Evoluciona pokemones", new ArrayList<Tipo_Actividad>(){{
+			add(ocio);
+			add(indoor);
+		}});
+		a9 = new Actividad("Ir al GYM", new ArrayList<Tipo_Actividad>(){{
+			add(ocio);
+			add(indoor);
+		}});
+		a10 = new Actividad("Correr", new ArrayList<Tipo_Actividad>(){{
+			add(ocio);
+			add(outdoor);
+			add(deporte);
+		}});
+		
 		//actividades realizadas
 		ar1 = new Actividad_Realizada(a1, new GregorianCalendar(2016,7,2,Calendar.HOUR,Calendar.MINUTE,Calendar.SECOND));
 		ar2 = new Actividad_Realizada(a1, new GregorianCalendar(2016,5,23,Calendar.HOUR,Calendar.MINUTE,Calendar.SECOND));
