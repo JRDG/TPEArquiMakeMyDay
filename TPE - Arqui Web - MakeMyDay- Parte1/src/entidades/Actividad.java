d29a75f2ff53eba0da978155e7e1dc02ddd309ff
whitefox <whitefox@Note-PBL2021>
2016-10-03 15:25:27 -0300

    jojo

--- TPE - Arqui Web - MakeMyDay- Parte1/src/entidades/Actividad.java
+++ TPE - Arqui Web - MakeMyDay- Parte1/src/entidades/Actividad.java
@@ -12,7 +12,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
-import javax.persistence.Table;
+import javax.persistence.Table; 
 
 
 @Entity
