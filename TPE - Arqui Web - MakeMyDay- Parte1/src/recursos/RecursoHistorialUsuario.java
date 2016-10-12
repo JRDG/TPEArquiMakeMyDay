40cef5e6d9e83b7741517765f5b44ca2140fb561
Joa <Joa@DESKTOP-R8PLF3A>
2016-10-11 23:15:05 -0300

    Areglado el metodo para buscar actividades entre fechas de un usuario

--- TPE - Arqui Web - MakeMyDay- Parte1/src/recursos/RecursoHistorialUsuario.java
+++ TPE - Arqui Web - MakeMyDay- Parte1/src/recursos/RecursoHistorialUsuario.java
@@ -1,22 +1,32 @@
 package recursos;
 
-import java.util.GregorianCalendar;
-import java.util.List;
+import java.security.Principal;
+import java.util.ArrayList;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.SecurityContext;
+
+import com.sun.security.auth.UserPrincipal;
 
 import entidades.Actividad;
 import entidades.HistorialUsuario;
 import persistencia.ActividadDAO;
 import persistencia.HistorialUsuarioDAO;
+import recursos.autorizacion.Secured;
+import utils.DatePair;
 
 @Path("/historial_usuario")
 public class RecursoHistorialUsuario {
+	@Context
+	SecurityContext securityContext;
+	
 	@GET
 	@Path("/{id}")
 	@Produces(MediaType.APPLICATION_JSON)
@@ -27,10 +37,13 @@ public class RecursoHistorialUsuario {
 
 	}
 	
-	@GET
+	@POST
+	@Secured
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
-	public HistorialUsuario getActividadesEntreFechas(){
-		HistorialUsuarioDAO.getInstance().buscarEntreFechas()
+	public ArrayList<HistorialUsuario> getActividadesEntreFechas(DatePair datePair){
+		Principal up = securityContext.getUserPrincipal();
+		String userName = up.getName();
+		return HistorialUsuarioDAO.getInstance().buscarEntreFechas(userName,datePair.getFromDate(),datePair.getToDate());
 	}
 }
