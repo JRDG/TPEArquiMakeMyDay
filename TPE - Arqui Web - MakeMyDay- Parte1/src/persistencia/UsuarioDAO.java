57fb23b657d402d04beb6dfa321716b2f72bcb6b
JRDG <joarod95@gmail.com>
2016-10-22 18:09:24 -0300

    Agregada la funcion Update y findIdByName<- sujeta a cambios jaja lol

--- TPE - Arqui Web - MakeMyDay- Parte1/src/persistencia/UsuarioDAO.java
+++ TPE - Arqui Web - MakeMyDay- Parte1/src/persistencia/UsuarioDAO.java
@@ -39,4 +39,36 @@ public class UsuarioDAO extends BaseJPADAO<Usuario> {
 		}
 		return usuarioBD;
 	}
+
+	public void update(Usuario u, int id) {
+		EntityManager entityManager = EntityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
+		Usuario uOriginal = entityManager.find(Usuario.class, id);
+		uOriginal.setNombre(u.getNombre());
+		uOriginal.setPassword(u.getPassword());
+		uOriginal.setApellido(u.getApellido());
+		uOriginal.setDNI(u.getDNI());
+		uOriginal.setFechaNacimiento(u.getFechaNacimiento());
+		entityManager.getTransaction().commit();
+		entityManager.close();
+
+	}
+
+	public int findIdByName(String userName) {
+		EntityManager em = EntityManagerFactory.createEntityManager();
+		int id = -1;
+		try {
+			String jpql = "SELECT u.id_usuario FROM Usuario u WHERE u.nombre = ?1";
+			Query query = em.createQuery(jpql, Usuario.class);
+			query.setParameter(1, userName);
+			id = (int) query.getSingleResult();
+		} catch (NoResultException e) {
+			System.out.println("no result");
+		} catch (Exception e) {
+			System.out.println(e);
+		} finally {
+			em.close();
+		}
+		return id;
+	}
 }
